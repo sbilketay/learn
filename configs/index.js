141a29666a994b4a516addf6d5fb7ca990a6812b
sbilgetay <wisecolt@gmail.com>
2019-10-03 10:58:06 +0300

    Add server mongodb url

--- configs/index.js
+++ configs/index.js
@@ -2,8 +2,9 @@ var config = {
     port: 3000,
     cookieExpirationTime: 1000 * 60 * 60 * 24 * 30, // 1 mounth
     tookenExpirationTime: 1000 * 60 * 60 * 24 * 30, //1 mounth
-    secret : "shhh",
-    mongodburl: 'mongodb://localhost:27017/auth',
+    secret: "shhh",
+    // mongodburl: 'mongodb://localhost:27017/auth',
+    mongodburl: 'mongodb://mongo:27017/authmongo' //server
 }
 
 module.exports = config
